a36967f8b1d97a16eb2ac57ac8835530c14b5deb
marthecharpy <marthe.charpy@gmail.com>
2019-03-21 11:40:06 +0100

    added shopping cart

--- cart.php
+++ cart.php
@@ -1,6 +1,13 @@
 <?php 
 session_start();
 
+$articles = [
+  32 => "M&M's&copy; cookies",
+  36 => "Chocolate chips",
+  46 => "Pecan nuts",
+  58 => "Chocolate cookie"
+];
+
 if (!isset($_SESSION['nom'])){
   header('Location: login.php'); 
   exit();
@@ -8,11 +15,8 @@ if (!isset($_SESSION['nom'])){
 require 'inc/head.php';
 ?>
 <section class="cookies container-fluid">
-    <div class="row">
-    	<h3><?php echo($_SESSION['nuts'])?></h3>
-    	<h3><?php echo($_SESSION['chips'])?></h3>
-    	<h3><?php echo($_SESSION['chocolate'])?></h3>
-    	<h3><?php echo($_SESSION['MMs'])?></h3>
-    </div>
+    <?php foreach($_SESSION['panier'] as $id){ ?>
+      <h3><?= $articles[$id] ?></h3>
+    <?php } ?>
 </section>
 <?php require 'inc/foot.php'; ?>
--- index.php
+++ index.php
@@ -9,71 +9,68 @@ if (!isset($_SESSION['nom'])){
   header('Location: login.php'); 
   exit();
 }
-$cookieNuts = $_POST['nuts'] ?? $_SESSION['nuts'] ?? "";
-$cookieChips = $_POST['chips'] ?? $_SESSION['chips'] ?? "";
-$cookieChocolate = $_POST['chocolate'] ?? $_SESSION['chocolate'] ?? "";
-$cookieMMs = $_POST['MMs'] ?? $_SESSION['MMs'] ?? "";
-$_SESSION['nuts'] = $cookieNuts;
-$_SESSION['chips'] = $cookieChips;
-$_SESSION['chocolate'] = $cookieChocolate;
-$_SESSION['MMs'] = $cookieMMs;
+
+if (isset($_GET['add_to_cart'])) {
+  // ensure $_SESSION['panier'] is set
+  $_SESSION['panier'] = $_SESSION['panier'] ?? array();
+
+  // add article
+  $_SESSION['panier'][] = $_GET['add_to_cart'];
+}
+
 require 'inc/head.php'; 
 ?>
 
 <section class="cookies container-fluid">
   <div class="row">
-    <form role="form" action="#" method="GET">
-      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-        <figure class="thumbnail text-center">
-          <img src="assets/img/product-46.jpg" alt="cookies choclate chips" class="img-responsive">
-          <figcaption class="caption">
-            <h3>Pecan nuts</h3>
-            <input type="text" name="nuts" value="Pecan nuts" hidden>
-            <p>Cooked by Penny !</p>
-            <button  type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart</button>              
-          </figcaption>
-        </figure>
-      </div>
-    </form>
-    <form role="form" action="#" method="GET">
-      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-        <figure class="thumbnail text-center">
-          <img src="assets/img/product-36.jpg" alt="cookies choclate chips" class="img-responsive">
-          <figcaption class="caption">
-            <input type="text" name="chips" value="Chocolate chips" hidden>
-            <h3>Chocolate chips</h3>
-            <p>Cooked by Bernadette !</p>
-            <button  type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart</button> 
-          </figcaption>
-        </figure>
-      </div>
-    </form>
-    <form role="form" action="#" method="GET">
-      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-        <figure class="thumbnail text-center">
-          <img src="assets/img/product-58.jpg" alt="cookies choclate chips" class="img-responsive">
-          <figcaption class="caption">
-            <input type="text" name="chocolate" value="Chocolate cookie" hidden>
-            <h3>Chocolate cookie</h3>
-            <p>Cooked by Bernadette !</p>
-            <button  type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart</button> 
-          </figcaption>
-        </figure>
-      </div>
-    </form>
-    <form role="form" action="#" method="GET">
-      <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-        <figure class="thumbnail text-center">
-          <img src="assets/img/product-32.jpg" alt="cookies choclate chips" class="img-responsive">
-          <figcaption class="caption">
-            <input type="text" name="MMs" value="M&M's&copy; cookies" hidden>
-            <h3>M&M's&copy; cookies</h3>
-            <p>Cooked by Penny !</p>
-            <button  type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart</button> 
-          </figcaption>
-        </figure>
-      </div>
-    </form>
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+      <figure class="thumbnail text-center">
+        <img src="assets/img/product-46.jpg" alt="cookies choclate chips" class="img-responsive">
+        <figcaption class="caption">
+          <h3>Pecan nuts</h3>
+          <p>Cooked by Penny !</p>
+          <a  href="?add_to_cart=46" class="btn btn-primary">
+            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
+          </a>
+        </figcaption>
+      </figure>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+      <figure class="thumbnail text-center">
+        <img src="assets/img/product-36.jpg" alt="cookies choclate chips" class="img-responsive">
+        <figcaption class="caption">
+          <h3>Chocolate chips</h3>
+          <p>Cooked by Bernadette !</p>
+          <a  href="?add_to_cart=36" class="btn btn-primary">
+            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
+          </a>
+        </figcaption>
+      </figure>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+      <figure class="thumbnail text-center">
+        <img src="assets/img/product-58.jpg" alt="cookies choclate chips" class="img-responsive">
+        <figcaption class="caption">
+          <h3>Chocolate cookie</h3>
+          <p>Cooked by Bernadette !</p>
+          <a  href="?add_to_cart=58" class="btn btn-primary">
+            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
+          </a>
+        </figcaption>
+      </figure>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+      <figure class="thumbnail text-center">
+        <img src="assets/img/product-32.jpg" alt="cookies choclate chips" class="img-responsive">
+        <figcaption class="caption">
+          <h3>M&M's&copy; cookies</h3>
+          <p>Cooked by Penny !</p>
+          <a  href="?add_to_cart=32" class="btn btn-primary">
+            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
+          </a>
+        </figcaption>
+      </figure>
+    </div>
   </div>
 </section>
 <?php require 'inc/foot.php'; ?>
